d789b01a36e06de675917ce345b2e022ad960528
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-08-09 01:11:02 +0200

    Pieces board started

--- src/components/ChessBoard.svelte
+++ src/components/ChessBoard.svelte
@@ -2,21 +2,21 @@
     import { onMount } from "svelte";
     import { fen } from "../lib/chess";
 
-    export const size = "400px";
-    export const start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    export let size = "400px";
+    export let start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-    fen.parse(start_fen);
+    const pieces = fen.parse(start_fen);
+    console.table(pieces)
 
     const ranks = ["H", "G", "F", "E", "D", "C", "B", "A"].reverse();
     const files = [8, 7, 6, 5, 4, 3, 2, 1];
 
-
 </script>
 
-<div id="board" class="board" style="width: {size}; height: {size};">
+<div class="board" style="width: {size}; height: {size};">
     {#each files as file}
         <div class="file" class:odd={file % 2 === 1}>
-            {#each ranks as rank, rank_index}
+            {#each ranks as rank}
                 <div class="cell">
                     <!-- File/rank notation -->
                     {#if file === 1}
@@ -25,16 +25,33 @@
                     {#if rank === "H"}
                         <span class="file-lbl">{file}</span>
                     {/if}
-                    <!-- Applicable piece -->
-                    <!-- <object data="/svg/pieces/B.svg" type="image/svg+xml" title="White bishop"></object> -->
                 </div>
             {/each}
         </div>
     {/each}
 </div>
 
-<div class="overlay" style="width: {size}; height: {size};">
-
+<div class="overlay" style="width: {size}; height: {size}; top: -{size}">
+    {#each files as file}
+        <div class="file" class:odd={file % 2 === 1}>
+            {#each ranks as rank, rank_index}
+                <div class="cell">
+                    <!-- Show a piece SVG if this cell is not empty -->
+                    {#if pieces[rank_index + file * 8] !== ""}
+                        <div class="piece">
+                            <object
+                                data="/svg/pieces/{pieces[rank_index - 1 + file * 8]}.svg"
+                                type="image/svg+xml"
+                                title="Chess piece ({pieces[rank_index - 1 + file * 8]})"
+                            >
+                                <!-- TODO: Fallback png -->
+                            </object>
+                        </div>
+                    {/if}
+                </div>
+            {/each}
+        </div>
+    {/each}
 </div>
 
 <style lang="scss">
@@ -91,6 +108,23 @@
                     margin-right: 3px;
                     margin-top: 3px;
                 }
+            }
+        }
+    }
+
+    .overlay {
+        position: relative;
+        display: grid;
+        grid-template-rows: repeat(8, 12.5%);
+
+        .file {
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(8, 12.5%);
+
+            .piece, .piece object {
+                width: 100%;
+                height: 100%;
             }
         }
     }
--- src/lib/chess.js
+++ src/lib/chess.js
@@ -1,48 +1,73 @@
-const valid_piece_rgx = /^[prnbqkPRNBQK]$/;
+/**
+ * @fileoverview Utility functions for making an interactive chess board.
+ * @author Andreas Kruhlmann
+ * @since 1.0.0
+ */
 
+/**
+ * Parses a FEN string and returns an array of pieces as represented by chars.
+ * Function does not validate impossible piece positions such as pawns before
+ * the starting line. The returned array uses the standard FEN notation for
+ * representing pieces as chars.
+ *
+ * @throws {ReferenceError} - FEN string not provided.
+ * @throws {TypeError} - FEN string argument was not a string.
+ * @throws {Error} - FEN string was invalid.
+ * @example
+ *     parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+ * @see {@link https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation | FEN notation specifications}
+ * @param {string} fen - FEN string to validate.
+ * @returns {string[]} - 64 length array of piece char representations.
+ */
 function parse_fen(fen) {
     if (!fen) {
         throw new ReferenceError("FEN string not provided");
     }
+
     if (typeof fen !== "string") {
         throw new TypeError("Argument was not a string");
     }
 
-    console.log(validate_fen(fen))
-}
+    // Regular expression for validating a piece char of a FEN string.
+    const fen_valid_piece_rgx = /^[prnbqkPRNBQK]$/;
+    // Regular expression for validating the castle permissions of a FEN string.
+    const fen_castle_perm_rxp = /^(KQ?k?q?|Qk?q?|kq?|q|-)$/;
+    // Regular expression for validating the side to move of a FEN string.
+    const fen_side_rxp = /^(w|b)$/;
+    // Regular expression for validating the en-passant square of a FEN string.
+    const en_passant_sqaure_rxp = /^(-|[abcdefgh][36])$/;
 
-function validate_fen(fen) {
-    // Tokens are wrapped in white space.
+    // Tokens are wrapped in white space (single space).
     const tokens = fen.split(/\s+/);
 
     // FEN string must contain six space-delimited fields.
     if (tokens.length !== 6) {
-        return false;
+        throw new Error("Invalid FEN; not enough fields");
     }
     // 6th field (50 move counter) must be a positive integer.
     if (isNaN(tokens[5]) || parseInt(tokens[5], 10) <= 0) {
-        return false;
+        throw new Error("Invalid FEN; invalid move counter (not a positive integer)");
     }
 
     // 5th field (ply count) must be a positive integer.
     if (isNaN(tokens[4]) || (parseInt(tokens[4], 10) < 0)) {
-        return false;
+        throw new Error("Invalid FEN; invalid ply count (not a positive integer");
     }
 
     // En-passant token must be a valid square or "-" if no en-passant
     // capture is avaliable.
-    if (!/^(-|[abcdefgh][36])$/.test(tokens[3])) {
-        return false;
+    if (!en_passant_sqaure_rxp.test(tokens[3])) {
+        throw new Error("Invalid FEN; invalid en-passant setting");
     }
 
     // Castle permissions must be a valid combination of KQkq.
-    if (!/^(KQ?k?q?|Qk?q?|kq?|q|-)$/.test(tokens[2])) {
-        return false;
+    if (!fen_castle_perm_rxp.test(tokens[2])) {
+        throw new Error("Invalid FEN; invalid castling permissions");
     }
 
     // 2nd field (side to move) must be either "w" or "b".
-    if (!/^(w|b)$/.test(tokens[1])) {
-        return false;
+    if (!fen_side_rxp.test(tokens[1])) {
+        throw new Error("Invalid FEN; invalid side to move (must be \"w\" or \"b\"");
     }
 
     // Board  validation.
@@ -50,20 +75,20 @@ function validate_fen(fen) {
 
     // There must be exactly 8 ranks.
     if (ranks.length !== 8) {
-        return false;
+        throw new Error("Invalid FEN; invalid number of ranks (must be exactly 8)");
     }
 
     // Cells found must be exactly 64 after the two dimensional for loop.
-    let cells_found = 0;
+    let board_pieces = [];
     // We also validate the rank itself; it must have exactly 8 cells.
-    let rank_cells_found;
+    let rank_pieces;
     // A number is not allowed to appear next to another number, so the last
     // cell type is being kept track of.
     let last_cell_type;
 
     for (const rank of ranks) {
         // Reset variables from last iteration.
-        rank_cells_found = 0;
+        rank_pieces = [];
         last_cell_type = "piece";
 
         // Iterate over every character in the rank.
@@ -73,46 +98,49 @@ function validate_fen(fen) {
                 // cells count.
 
                 // Validate the piece character.
-                if (!valid_piece_rgx.test(char)) {
-                    return false;
+                if (!fen_valid_piece_rgx.test(char)) {
+                    throw new Error(`Invalid FEN; piece char was not recognized (found "${char}")`);
                 }
 
-                rank_cells_found ++;
+                rank_pieces.push(char);
                 last_cell_type = "piece";
             } else {
                 // Runs if the selected char is an empty space indicator.
 
                 // Number cells cannot appear next to one another.
                 if (last_cell_type === "number") {
-                    return false;
+                    throw new Error("Invalid FEN; cannot contain two whitespace indicators in sequence");
                 }
 
                 // A number represents empty spacec according to it's value, so
-                // it is parsed as an integer and added to the cells found.
-                rank_cells_found += parseInt(char, 10);
+                // it is parsed as an integer and the amount is added as empty
+                // pieces to the pieces list.
+                const empty_cells = parseInt(char, 10);
+                for (let i = 0; i < empty_cells; i++) {
+                    rank_pieces.push("");
+                }
                 last_cell_type = "empty";
             }
         }
 
         // A rank must have exactly 8 cells.
-        if (rank_cells_found !== 8) {
-            return false;
+        if (rank_pieces.length !== 8) {
+            throw new Error(`Invalid FEN; rank did not have exactly 8 cells (was ${rank_cells_found} on rank ${rank + 1})`);
         }
 
         // Add the cell count of this rank to the total cell count.
-        cells_found += rank_cells_found;
+        board_pieces = [...board_pieces, ...rank_pieces];
     }
 
     // A board must have exactly 64 cells.
-    if (cells_found !== 64) {
-        return false;
+    if (board_pieces.length !== 64) {
+        throw new Error(`Invalid FEN; amount of cells was not 64 (was ${cells_found})`);
     }
 
     // All tests passed.
-    return true;
+    return board_pieces;
 }
 
 export const fen = {
     parse: parse_fen,
-    validate: validate_fen,
 }
