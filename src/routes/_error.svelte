ef4b6dcf6bf575f78b7ad992e01e1e7cd9225d2a
Andreas Kruhlmann <andreas@kruhlmann.com>
2019-08-11 21:21:23 +0200

    Added error page

---
+++ src/routes/_error.svelte
@@ -0,0 +1,40 @@
+<div class="image-wrapper">
+    <img src="/svg/thonk.svg" alt="thonk">
+    <div class="error">
+        <span class="code">404</span>
+        <div class="message">File not found</div>
+    </div>
+</div>
+
+<style lang="scss">
+    @import "../scss/vars";
+
+    .image-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        background-color: $viewport-lighter;
+
+        img {
+            width: 65%;
+            height: 65%;
+        }
+
+        .error {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            color: $text-default;
+
+            .code {
+                font-size: 92px;
+            }
+
+            .message {
+                font-size: 66px;
+            }
+        }
+    }
+</style>
